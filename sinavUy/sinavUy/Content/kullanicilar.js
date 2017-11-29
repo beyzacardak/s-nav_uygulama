676637a7fff00f14703757aa481f0b45ce179f64
BEM <seymayetisen@gmail.com>
2017-11-29 10:56:47 +0300

    h

---
+++ sinavUy/sinavUy/Content/kullanicilar.js
@@ -0,0 +1,27 @@
+﻿[
+    {
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",
+        
+        "soyisim": "Aygün",
+        "profil_resmi": "r1.jpg",
+        "d_tarihi": "01.01.1985",
+        "mezun_oldgu_okul": "Gazi Üni",
+        "fakulte": "ESEF",
+        "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        "konular":["c#","javascript","html"]
+    },
+    {
+        "kullanici_adi": "bem",
+        "sifre": "987654",
+        "isim": "Betul",
+        "soyisim": "Tasdemir",
+        "profil_resmi": "r2.jpg",
+        "d_tarihi": "01.01.1985",
+        "mezun_oldgu_okul": "Gazi Üni",
+        "fakulte": "ESEF",
+        "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        "konular": ["c#", "javascript", "html"]
+    }
+]
---
+++ sinavUy/sinavUy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sinavUy.MvcApplication" Language="C#" %>
